108fea711d609ecfbbd55248b6db9c1830d85bb0
Opilane TTHK <opilane@tthk.ee>
2024-02-20 12:43:20 +0200

    sdasd

--- MHKHommikuHelbed/Models/Helbed.cs
+++ MHKHommikuHelbed/Models/Helbed.cs
@@ -3,7 +3,7 @@
 public class Helbed
 {
     public string Name { get; set; }
-    public string Image { get; set; }
+    public string PFP { get; set; }
     public string Description { get; set; }
     public List<string> Images { get; set; }
 }

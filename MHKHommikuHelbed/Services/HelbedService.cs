86c4a2cd03c2ef0adb11a0c8f450eafa3d1bcb4d
Opilane TTHK <opilane@tthk.ee>
2024-02-20 12:41:19 +0200

    Algus

---
+++ MHKHommikuHelbed/Services/HelbedService.cs
@@ -0,0 +1,26 @@
+﻿using MHKHommikuHelbed.models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    internal class HelbedService
+    {
+        private static List<Helbed> helbeds = new()
+        {
+            new()
+            {
+                Name = "Name",
+                PFP = "image.png",
+                Description = "Description",
+                Images = new()
+                {
+                    "Image1.png",
+                    "Image2.png",
+                    "Image3.png",
+                }
+            }
+        };
+    }
+}

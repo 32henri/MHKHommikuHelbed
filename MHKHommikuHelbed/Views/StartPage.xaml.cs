46a42e3f61e264633c89e61241cfa0b710beb510
Opilane TTHK <opilane@tthk.ee>
2024-02-21 11:55:45 +0200

    StartPage-i nupp

---
+++ MHKHommikuHelbed/Views/StartPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace Views;
+
+public partial class StartPage : ContentPage
+{
+	public StartPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}

86f5ff496ccc07d80ad423c8636e932bb91b4273
Opilane TTHK <opilane@tthk.ee>
2024-03-11 09:18:57 +0200

    dronhhnerhn

---
+++ MHKHommikuHelbed/obj/Debug/net6.0-android/android/src/crc648a6755937075c67d/MainActivity.java
@@ -0,0 +1,46 @@
+package crc648a6755937075c67d;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("MHKHommikuHelbed.MainActivity, MHKHommikuHelbed", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("MHKHommikuHelbed.MainActivity, MHKHommikuHelbed", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("MHKHommikuHelbed.MainActivity, MHKHommikuHelbed", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
